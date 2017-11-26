a1c75c938fddc2192291942efd430166a6255350
Robbe Gillis <robbe.gillis@immalle.eu>
2017-11-26 21:34:44 +0100

    Commit 1

--- MyFileBrowser_1/Program.cs
+++ MyFileBrowser_1/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
 namespace MyFileBrowser_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                ShowDefault();
+            }
+            else
+            {
+                switch (args[0])
+                {
+                    case "-c":
+                        ShowCurrentDirectory();
+                        break;
+                    case "-f":
+                        ShowAllFilesInCurrentDirectory();
+                        break;
+                    case "-d":
+                        ShowAllSubDirectories();
+                        break;
+                    case "-h":
+                        ShowHelp();
+                        break;
+
+                    default:
+                        Console.WriteLine("Onbekend argument");
+                        break;
+                }
+            }
         }
+        static void ShowDefault()
+        {
+
+        }
+        static void ShowCurrentDirectory()
+        {
+
+
+        }
+        static void ShowAllFilesInCurrentDirectory()
+        {
+
+        }
+        static void ShowAllSubDirectories()
+        {
+
+        }
+        static void ShowHelp()
+        {
+
+        }
+
     }
+
 }
