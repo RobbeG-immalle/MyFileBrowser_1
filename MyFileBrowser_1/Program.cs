8bfc5855a716b1736c5fcdd6c8cf86770c943b78
Robbe Gillis <robbe.gillis@immalle.eu>
2017-11-26 21:32:53 +0100

    Add project files.

---
+++ MyFileBrowser_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFileBrowser_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
